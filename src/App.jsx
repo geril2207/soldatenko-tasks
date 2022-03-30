c18bbc3f34590ac349b99e93860096e9a4d8e2ba
Мясников Егор Евгеньевич <Myasnikov_EE@radius.belgorod.com>
2022-03-30 11:03:02 +0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,36 @@ import './App.css';
 import Registration from './components/Registration'
 import Autorization from './components/Autorization'
 import PhotoOperations from './components/PhotoOperations'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
 
-      <body>
-        <PhotoOperations/>
-    
-  
-      </body> 
-    </div>
+        <body>
+          <Switch>
+            <Route path="/photo">
+              <PhotoOperations />
+            </Route>
+            <Route path="/register">
+              <Registration />
+            </Route>
+            <Route path="/auth">
+              <Autorization />
+            </Route>
+            <Redirect to="/auth"/>
+          </Switch>
+
+        </body>
+      </div>
+    </Router>
   );
 }
 
